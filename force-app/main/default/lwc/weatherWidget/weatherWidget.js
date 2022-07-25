934dab89a3b4c1c8bec8821ed977378cb4623e3c
Pawel Kondraciuk <kondraciukpawel@praintl.com>
2022-07-25 16:13:21 +0200

    Implemented weather widget component

--- force-app/main/default/lwc/weatherWidget/weatherWidget.js
+++ force-app/main/default/lwc/weatherWidget/weatherWidget.js
@@ -2,8 +2,46 @@
  * Created by pawko on 25.07.2022.
  */
 
-import {LightningElement} from 'lwc';
+import {LightningElement, track} from 'lwc';
+import getWeather from '@salesforce/apex/WeatherWidgetController.getWeather'
 
 export default class WeatherWidget extends LightningElement {
+    @track searchKey;
+    isMetric = true;
+    location;
+    weather;
+    error;
+
+    units = {
+        heat:'',
+        speed:'',
+        distance:'',
+        smallDistance:''
+    }
+    handleGetWeather(){
+        this.searchKey = this.template.querySelector('lightning-input').value;
+        getWeather({query: this.searchKey})
+            .then((result) => {
+                this.location = result.location;
+                this.weather = result.forecast.forecastDay[0].day;
+                this.error = result.error;
+                this.setUnits();
+                console.log(this.location);
+                console.log(this.weather);
+                console.log(this.error);
+            })
+    }
+
+    setUnits(){
+        this.units.heat = this.isMetric ? '°C' : '°F';
+        this.units.speed = this.isMetric ? 'km/h' : 'MPH';
+        this.units.distance = this.isMetric ? 'km' : 'Miles';
+        this.units.smallDistance = this.isMetric ? 'mm' : 'in';
+    }
+
+    handleChange(event) {
+        this.isMetric = event.target.checked;
+        this.setUnits();
+    }
 
 }
